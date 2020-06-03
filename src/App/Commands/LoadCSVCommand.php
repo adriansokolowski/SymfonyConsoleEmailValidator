c1217e5392e7cc37f59dcb06b8a9ec61be6364a1
adriansokolowski <37288976+adriansokolowski@users.noreply.github.com>
2020-06-03 17:29:59 +0200

    Validate emails

--- src/App/Commands/LoadCSVCommand.php
+++ src/App/Commands/LoadCSVCommand.php
@@ -19,27 +19,30 @@ class LoadCSVCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('Loaded file: ' . $input->getArgument('file'));
-        $array = $fields = array(); 
-        $i = 0;
-        $handle = @fopen($input->getArgument('file'), "r");
-        if ($handle) {
-            while (($row = fgetcsv($handle, 4096)) !== false) {
-                if (empty($fields)) {
-                    $fields = $row;
-                    continue;
-                }
-                foreach ($row as $k=>$value) {
-                    $array[$i][$fields[$k]] = $value;
-                    $output->writeln($array[$i][$fields[$k]]);
-                }
-                $i++;
-            }
-            if (!feof($handle)) {
-                echo "Error: unexpected fgets() fail\n";
-            }
-            fclose($handle);
+        $rows = array_map('str_getcsv', file($input->getArgument('file')));
+        $csv = [];
+        $validEmails = [];
+        $invalidEmailsSyntax = [];
+        $invalidEmailsHost = [];
+        foreach($rows as $row) {
+            $csv[] = $row;
         }
-
+        foreach($csv as $k => $v) {
+            $v = implode('', $v);
+            if (filter_var($v, FILTER_VALIDATE_EMAIL)) {
+                $domain = substr($v, strpos($v, '@') + 1);
+                if  (checkdnsrr($domain) !== FALSE) {
+                    $validEmails[] = $v;
+                } else {
+                    $invalidEmailsHost[] = $v;
+                }
+                $validEmails[] = $v;
+            } else {
+                $invalidEmailsSyntax[] = $v;
+                $output->writeln($v);
+            }
+        }
+        
         return Command::SUCCESS;
     }
 
